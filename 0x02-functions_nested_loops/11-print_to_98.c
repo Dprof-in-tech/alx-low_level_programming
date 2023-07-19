d620c51228dd314e95c15f8db20662c4bf1063c1
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-07-19 22:25:28 +0000

    completed task 11- print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,26 +10,25 @@
 
 void print_to_98(int n)
 {
-	int i, j;
+	int i;
 
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
+			printf("%d", i);
 			if (i != 98)
-				printf("%d,", i);
-			else if( i == 98)
-				printf("%d\n", i);
+			printf(", ");
 		}
 	}
-	else if (n >= 98)
+	else
 	{
-		for (j =n; j >= 98; j++)
+		for (i = n; i >= 98; i--)
 		{
-			if (j != 98)
-				printf("%d,", j);
-			else if (j == 98)
-				printf("%d\n", j);
+			printf("%d", i);
+			if (i != 98)
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
