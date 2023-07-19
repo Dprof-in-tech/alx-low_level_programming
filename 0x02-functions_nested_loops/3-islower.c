4d0f8cc580094547f0792d7a6de3a5e4ffec424d
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-07-19 04:25:54 +0000

    completed task 3- is_lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main- entry point
+ *
+ * Description- function checks if alphabet is in lowercase
+ *
+ * Return 1 if c is lowercae
+ * Return 0 if c is not lowercase
+ */
+
+int _islower(int c)
+{
+	if(c>=97 && c<= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
