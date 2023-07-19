fec73c8e2245056a5c5be677fdbe6cacd4951a3b
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-07-19 23:06:06 +0000

    completed advanced task - 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,25 +7,45 @@
  */
 void print_times_table(int n)
 {
+	int i, j, k;
+	
 	if (n >= 0 && n <= 15)
 	{
-		int i, j, product;
-
 		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				product = i * j;
-				if (j != 0)
-					printf(", ");
-				if (product < 10)
-					printf("  ");
-				else if (product < 100)
-					printf(" ");
-				
-				printf("%d", product);
-			}
-			printf("\n");
-		}
-	}
+                        {
+                                for (j = 0; j <= n; j++)
+                                {
+                                        k = j * i;
+                                        if (j == 0)
+                                        {
+                                                _putchar(k + '0');
+                                        } 
+					else if (k < 10 && j != 0)
+                                        {
+                                                _putchar(',');
+                                                _putchar(' ');
+                                                _putchar(' ');
+                                                _putchar(' ');
+                                                _putchar(k + '0');
+                                        }
+				       	else if (k >= 10 && k < 100)
+                                        {
+                                                _putchar(',');
+                                                _putchar(' ');
+                                                _putchar(' ');
+                                                _putchar((k / 10) + '0');
+					       	_putchar((k % 10) + '0');
+                                        }
+				       	else if (k >= 100)
+                                        {
+                                                _putchar(',');
+                                                _putchar(' ');
+                                                _putchar((k / 100) + '0');
+                                                _putchar(((k / 10) % 10) + '0');
+                                                _putchar((k % 10) + '0');
+                                        }
+                                }
+                                _putchar('\n');
+                        }
+                }
 }
