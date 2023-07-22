91e92c2314491f1413c16c167bcba76eed30975d
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-07-22 05:09:30 +0000

    completed task 3 of c-debugging - 0>972?

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,8 +2,9 @@
 
 /**
 * main - causes an infinite loop
-* Return: 0 
+* Return: 0
 */
+
 int main(void)
 {
 	int i;
@@ -12,7 +13,7 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)
+	/**while (i < 10)
 	*{
 	*	putchar(i);
 	}*/
