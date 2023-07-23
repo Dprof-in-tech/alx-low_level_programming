bc3973eb6447a2364a7335d87d4d854153dcc4ed
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-07-23 23:58:17 +0000

    completed task 9 - fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main- Function to print fizz-buzz
+ *
+ * Return: Always 0;
+ */
 
 int main(void)
 {
@@ -23,7 +28,5 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-
-	printf("\n");
-	return 0;
+	return (0);
 }
