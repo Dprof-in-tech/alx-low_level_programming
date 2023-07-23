b5502e79a160d9cb01d66cac076097dcb576ace6
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-07-23 21:04:43 +0000

    completed task 2- multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * mul - function to multiply two integers
- * @b - integer to be multiplied
- * @a - integer to be multiplied
+ * @b - Integer to be multiplied
+ * @a - Integer to be multiplied
  * Return: Always 0
  */
 int mul(int a, int b)
