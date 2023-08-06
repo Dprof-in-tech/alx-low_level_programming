1b4733e4ae7d7793d5ef16e113e1b0e167de26d1
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-06 21:47:20 +0000

    completed task 1 0f argc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - function to print out the argument count
+ * @argc: the argument count to be printed
+ * @argv: the array containing the arguments
+ *
+ * return - O always success
+ */
+
+int main(int argc, char **argv)
+{
+	if (*argv++)
+	{
+		printf ("%d\n", --argc);
+	}
+	exit (EXIT_SUCCESS);
+}
