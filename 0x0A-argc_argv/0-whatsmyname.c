7a5913a14e70228d2cd464a6dbe56e6789c0ad80
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-06 21:30:47 +0000

    completed task 0 of argc print file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * main - function to print filename
+ *
+ * @argc: contains the number of items in the array argv
+ * @argv: array containing the name of file to print
+ *
+ * return - Always 0 successful
+ */
+
 int main(int argc, char **argv)
 {
-	while(argc--)
-		printf("%s\n", *argv++);
-	exit(EXIT_SUCCESS);
+	while (argc--)
+		printf ("%s\n", *argv++);
+	exit (EXIT_SUCCESS);
 }
