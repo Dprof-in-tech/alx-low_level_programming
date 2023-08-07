ff0a6f97603811440ae9fa77194bd1a39ff127cb
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-07 02:55:07 +0000

    task 3 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main-the function that prints out the results of the numbers
+ * @argc: contains the array count
+ * @argv: array containing input characters\
+ * @num1: input number 1
+ * @num2: input number 2
+ * @result: result of mathematicla operations between num 1 and num 2
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+		printf("Error\n");
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+
+		result = num1 * num2;
+
+		printf("%d\n", result);
+	}
+	return (0);
+}
