dfe26f3e1d66d557b3f2ab52ebfac5df259221f4
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-07 03:36:02 +0000

    task 4 completed

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main- Function that prints the addition of arguments
+ * @argc: contains the number of arguments
+ * @argv: array containing the arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int result = 0;
+	int a = 1;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (a < argc)
+	{
+		if (!is_positive_integer(argv[a]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		result += atoi(argv[a]);
+		a++;
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
+
+int is_positive_integer(const char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+
+	return (1);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 int main(int argc, char **argv);
+int is_positive_integer(const char *str);
 #endif
