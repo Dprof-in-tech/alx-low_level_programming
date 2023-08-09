b7f7751bcbc12fc657dcf808cc0470c9eeec2d06
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-09 05:51:15 +0000

    second advanced task malloc-free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -61,11 +61,13 @@ char **strtow(char *str)
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
+				{
 					*tmp++ = str[start++];
 					*tmp = '\0';
 					matrix[k] = tmp - c;
 					k++;
 					c = 0;
+				}
 			}
 		}
 		else if (c++ == 0)
