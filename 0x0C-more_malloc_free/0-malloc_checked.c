5829b967487f0a178ad07319dd23c561c9a7b6ef
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-10 13:59:32 +0000

    edited the code

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,12 +11,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	char *str;
 
-	ptr = malloc(b);
+	str = malloc(sizeof(char) * b);
 
-	if (ptr == NULL)
+	if (str == NULL)
 		exit(98);
 
-	return (ptr);
+	return (str);
 }
