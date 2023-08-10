6234253357f5b4862813ea50c6704e4d0149b350
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-10 13:31:00 +0000

    completed task 0 of more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Function to check malloc for a string and return pointer
+ * @b: input integer
+ *
+ * Return: Always 0 success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *string;
+
+	string = malloc(sizeof(char));
+
+	if (string == NULL)
+		exit(98);
+
+	return (string);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the folder for my alx tasks on more operattion using malloc and free functions.
