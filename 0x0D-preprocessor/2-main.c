ce36276995cc293b1317857cc9914a24d5acfe08
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-16 05:05:04 +0000

    completed task 2 of preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function to print source file name
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	putchar('__FILE__\n');
+	return (0);
+}
