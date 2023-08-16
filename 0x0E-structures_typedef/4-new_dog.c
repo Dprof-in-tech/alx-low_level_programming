7c137cc4255e1359a0a9f82f6cd4f0e7a9d67aa0
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-16 21:07:43 +0000

    completed task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,7 +19,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-		free(new_dog);
 		return (NULL);
 
 	new_dog->name = malloc(strlen(name) + 1);
@@ -33,6 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
