e9180c84539b2d099e72707b03aa4b611b3644a3
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-16 07:37:13 +0000

    completed task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+
 /**
  * init_dog - function to initialize dog structure
  * @d: dog structure
@@ -13,10 +15,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d.name = name;
-		d.age = age;
-		d.owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
-	return (0);
 }
