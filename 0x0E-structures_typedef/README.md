2e9b9614d6eec08cf52685e81e054cad346f73f9
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-16 07:02:17 +0000

    completed task 0 of structures

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is the folder for my learning c-structures and typedef from the Alx SE course
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - structure containing the dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
