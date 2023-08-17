953aef2141deeed319b98c8bfc2dc00b1a6efabe
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-17 02:15:23 +0000

    completetd task 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int_index - Function to print the index of a numbef
+ * @array: input array
+ * @size: size of array
+ * @cmp: function pointer
+ *
+ * Return: Always 0 success
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+
+	}
+	return (-1);
+}
