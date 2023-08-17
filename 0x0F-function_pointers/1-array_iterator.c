8ff5aa46bfcf0be7dc731d2f767d6cfb34fd0b0f
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-17 01:59:23 +0000

    completed task one

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator -  Function to print array
+ * @array: array of numbers
+ * @action: function pointer
+ * @size: size of arrays
+ *
+ * Return: none;
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
