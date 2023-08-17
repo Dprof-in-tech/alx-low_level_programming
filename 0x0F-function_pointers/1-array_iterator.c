3900dfa4d6c580239b7e386aa80e290750a04ad9
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-17 06:30:29 +0000

    edited function pointers.h

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,5 +15,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	for (i = 0; i < size; i++)
-		action(array[i]);
+	{
+		if (array[i] != NULL)
+		{
+			action(array[i]);
+		}
+	}
 }
