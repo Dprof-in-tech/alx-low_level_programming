b25ebf8d125bdfae993652b78ab6914561ec0874
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-18 02:36:44 +0000

    completed task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Function to print numbers
+ * @separator: The character to seperate the printed numbers
+ * @n: 1st input number
+ *
+ * Return: Always 0 success
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list number;
+	unsigned int nxtnum;
+
+	va_start(number, n);
+	if (separator != NULL)
+		printf("%d", va_arg(number, int));
+
+	for (nxtnum = 1; nxtnum < n; nxtnum++)
+	{
+		printf("%s%d", separator, va_arg(number, int));
+	}
+
+	va_end(number);
+	printf("\n");
+}
