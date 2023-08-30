93391a0dc0f96a71d3c733c460c9c8212459ce57
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-08-30 04:52:23 +0000

    completed task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * pop_listint - Function to delete node
+ * @head - List head
+ *
+ * Return: Always 0 success
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int str;
+
+	if (*head == NULL)
+		return (0);
+
+	ptr = (*head)->next;
+	str = (*head)->n;
+
+	free(ptr);
+
+	return (str);
+}
