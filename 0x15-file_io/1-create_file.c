49c42ac9671b1e1336d6d9993574d5f58e1590b9
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2023-09-06 01:44:56 +0000

    completed task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+/**
+ * create_file - Function to create a new empty file
+ * @filename: name of file to create
+ * @text_content: desired content of created file
+ *
+ * Return: created file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(file, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	close(file);
+	return (1);
+}
